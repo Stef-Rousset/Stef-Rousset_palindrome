8dad960f5c68ab5def03759da298cb8ff8a5b4ae
stephanie rousset <strabon@club-internet.fr>
2021-02-04 16:11:15 +0100

    tests ok

--- lib/Stef/Rousset_palindrome.rb
+++ lib/Stef/Rousset_palindrome.rb
@@ -1,8 +1,26 @@
 require "Stef/Rousset_palindrome/version"
 
-module Stef
-  module RoussetPalindrome
-    class Error < StandardError; end
-    # Your code goes here...
+# module Stef
+#   module RoussetPalindrome
+#     class Error < StandardError; end
+#     # Your code goes here...
+#   end
+# end
+  class String
+
+    # Returns true for a palindrome, false otherwise.
+    def palindrome?
+      processed_content == processed_content.reverse
+    end
+
+    def letters
+      self.gsub(/[\W\s]/, '')
+    end
+
+    private
+
+      # Returns content for palindrome testing.
+      def processed_content
+        self.letters.downcase
+      end
   end
-end
